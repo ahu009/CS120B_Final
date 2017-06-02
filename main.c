517f03fd7432b2fa6a9288010832e9ef05494f50
ahu009 <ahu009@ucr.edu>
2017-06-02 16:09:57 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,127 +1,96 @@
-/*
- * GccApplication1.cp/*	Partner(s) Name & E-mail: Dillon Sio dsio001@ucr.edu
- *	Lab Section: 022
- *	Assignment: Lab #6  Exercise #1
- *	Exercise Description: [optional - include for your own benefit]
- *	
- *	I acknowledge all content contained herein, excluding template or example
- *	code, is my own original work.
- */
-
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include "io.c"
 
-volatile unsigned char TimerFlag = 0; // TimerISR() sets this to 1. C programmer should clear to 0.
+volatile unsigned char TimerFlag = 0;
+unsigned long _avr_timer_M = 1; 
+unsigned long _avr_timer_cntcurr = 0; 
+unsigned char column_val = 0x80;
+unsigned char column_sel = 0x7F;
 
-// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
-unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 ms.
-unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks
-
-
-
-//***********************************************************************//
-//                Analog to Digital Setup Function                       //
-//***********************************************************************//
+//Code Taken From (Driver for Joystick):
 //http://www.embedds.com/interfacing-analog-joystick-with-avr/
 void InitADC(void)
 {
 	ADMUX|=(1<<REFS0);
-	ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); //ENABLE ADC, PRESCALER 128
+	ADCSRA|=(1<<ADEN)|(1<<ADPS0)|(1<<ADPS1)|(1<<ADPS2); 
 }
 uint16_t readadc(uint8_t ch)
 {
-	ch&=0b00000111;         //ANDing to limit input to 7
-	ADMUX = (ADMUX & 0xf8)|ch;  //Clear last 3 bits of ADMUX, OR with ch
-	ADCSRA|=(1<<ADSC);        //START CONVERSION
-	while((ADCSRA)&(1<<ADSC));    //WAIT UNTIL CONVERSION IS COMPLETE
-	return(ADC);        //RETURN ADC VALUE
+	ch&=0b00000111;         
+	ADMUX = (ADMUX & 0xf8)|ch;  
+	ADCSRA|=(1<<ADSC);        
+	while((ADCSRA)&(1<<ADSC));    
+	return(ADC);    
 }
+//Driver for Joystick Ends Here
 
-
+//Shift Register Transmit Data Functions Taken from Supplemental Lab (Modified)
 void transmit_data_red(unsigned char data) {
 	int i;
 	for (i = 0; i < 8 ; ++i) {
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
 		PORTD = 0x08;
-		// set SER = next bit of data to be sent.
 		PORTD |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
 		PORTD |= 0x02;
 	}
-	// set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
 	PORTD |= 0x04;
-	// clears all lines in preparation of a new transmission
 	PORTD = 0x00;
 }
 
 void transmit_data_blue(unsigned char data) {
 	int i;
 	for (i = 0; i < 8 ; ++i) {
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
 		PORTA = 0x08;
-		// set SER = next bit of data to be sent.
+
 		PORTA |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+
 		PORTA |= 0x02;
 	}
-	// set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+
 	PORTA |= 0x04;
-	// clears all lines in preparation of a new transmission
 	PORTA = 0x00;
 }
 
 void transmit_data(unsigned char data) {
 	int i;
 	for (i = 0; i < 8 ; ++i) {
-		// Sets SRCLR to 1 allowing data to be set
-		// Also clears SRCLK in preparation of sending data
 		PORTB = 0x08;
-		// set SER = next bit of data to be sent.
+	
 		PORTB |= ((data >> i) & 0x01);
-		// set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+
 		PORTB |= 0x02;
 	}
-	// set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+
 	PORTB |= 0x04;
-	// clears all lines in preparation of a new transmission
+	
 	PORTB = 0x00;
 }
+//Shift register transmit data functions end here
 
-
-
+//Buzzer Drivers From Lab 8
 void set_PWM(double frequency) {
 
 	static double current_frequency;
 	if (frequency != current_frequency) {
 
-		if (!frequency) TCCR3B &= 0x08; //stops timer/counter
-		else TCCR3B |= 0x03; // resumes/continues timer/counter
+		if (!frequency) TCCR3B &= 0x08; 
+		else TCCR3B |= 0x03; 
 		
-		// prevents OCR3A from overflowing, using prescaler 64
-		// 0.954 is smallest frequency that will not result in overflow
 		if (frequency < 0.954) OCR3A = 0xFFFF;
 		
-		// prevents OCR3A from underflowing, using prescaler 64
 		else if (frequency > 31250) OCR3A = 0x0000;
 		
-		// set OCR3A based on desired frequency
 		else OCR3A = (short)(8000000 / (128 * frequency)) - 1;
 
-		TCNT3 = 0; // resets counter
+		TCNT3 = 0;
 		current_frequency = frequency;
 	}
 }
 
 void PWM_on() {
 	TCCR3A = (1 << COM3A0);
-	// COM3A0: Toggle PB6 on compare match between counter and OCR3A
 	TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
-	// WGM32: When counter (TCNT3) matches OCR3A, reset counter
-	// CS31 & CS30: Set a prescaler of 64
 	set_PWM(0);
 }
 
@@ -129,67 +98,79 @@ void PWM_off() {
 	TCCR3A = 0x00;
 	TCCR3B = 0x00;
 }
-
+//Buzzer Driver Ends here
 
 void TimerOn() {
-	// AVR timer/counter controller register TCCR1
-	TCCR1B = 0x0B;// bit3 = 0: CTC mode (clear timer on compare)
-	// bit2bit1bit0=011: pre-scaler /64
-	// 00001011: 0x0B
-	// SO, 8 MHz clock or 8,000,000 /64 = 125,000 ticks/s
-	// Thus, TCNT1 register will count at 125,000 ticks/s
+	TCCR1B = 0x0B;
+	
+	OCR1A = 125;	
 
-	// AVR output compare register OCR1A.
-	OCR1A = 125;	// Timer interrupt will be generated when TCNT1==OCR1A
-	// We want a 1 ms tick. 0.001 s * 125,000 ticks/s = 125
-	// So when TCNT1 register equals 125,
-	// 1 ms has passed. Thus, we compare to 125.
-	// AVR timer interrupt mask register
-	TIMSK1 = 0x02; // bit1: OCIE1A -- enables compare match interrupt
+	TIMSK1 = 0x02;
 
-	//Initialize avr counter
 	TCNT1=0;
 
 	_avr_timer_cntcurr = _avr_timer_M;
-	// TimerISR will be called every _avr_timer_cntcurr milliseconds
 
-	//Enable global interrupts
-	SREG |= 0x80; // 0x80: 1000000
+	SREG |= 0x80; 
 }
 
 void TimerOff() {
-	TCCR1B = 0x00; // bit3bit1bit0=000: timer off
+	TCCR1B = 0x00; 
 }
 
 void TimerISR() {
 	TimerFlag = 1;
 }
 
-// In our approach, the C programmer does not touch this ISR, but rather TimerISR()
 ISR(TIMER1_COMPA_vect) {
-	// CPU automatically calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
-	_avr_timer_cntcurr--; // Count down to 0 rather than up to TOP
-	if (_avr_timer_cntcurr == 0) { // results in a more efficient compare
-		TimerISR(); // Call the ISR that the user uses
+	_avr_timer_cntcurr--; 
+	if (_avr_timer_cntcurr == 0) { 
+		TimerISR(); 
 		_avr_timer_cntcurr = _avr_timer_M;
 	}
 }
 
-// Set TimerISR() to tick every M ms
 void TimerSet(unsigned long M) {
 	_avr_timer_M = M;
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b)
+void LightLED(unsigned char x, unsigned char y)
 {
-	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+	unsigned char temp_y = 0x80; 
+	unsigned char temp_x = 0x7F; 
+	
+	for(int i = 1; i < x; i++){
+		temp_x = (temp_x >> 1) | 0x80;
+	}
+	for(int i = 1; i < y; i++){
+		temp_y = temp_y >> 1;
+	}
+	
+	column_sel = column_sel & temp_x;
+	column_val = column_val | temp_y;
+	transmit_data(column_val); 
+	transmit_data_blue(column_sel); 
+	return;
 }
 
-unsigned char GetBit(unsigned char x, unsigned char k)
+void UnlightLED(unsigned char x, unsigned char y)
 {
+	unsigned char temp_y = 0x7F; 
+	unsigned char temp_x = 0x80; 
 	
-	return ((x & (0x01 << k)) != 0);
+	for(int i = 1; i < x; i++){
+		temp_x = (temp_x >> 1);
+	}
+	for(int i = 1; i < y; i++){
+		temp_y = (temp_y >> 1) | 0x80;
+	}
+	
+	column_sel = column_sel | temp_x;
+	column_val = column_val & temp_y;
+	transmit_data(column_val); // PORTB selects column to display pattern
+	transmit_data_blue(column_sel); // PORTA displays column pattern
+	return;
 }
 
 unsigned char ButtonA1;
@@ -204,15 +185,15 @@ enum States{Init, ReadYAnalogInput, ReadXAnalogInput} state;
 
 void ButtonTick()
 {
-	static unsigned char score = 5;
 	const unsigned char* string = reinterpret_cast<const unsigned char *>("Score: ");
 	const unsigned char* string1 = reinterpret_cast<const unsigned char *>("Right");
 	const unsigned char* string2 = reinterpret_cast<const unsigned char *>("Left");
 	const unsigned char* string3 = reinterpret_cast<const unsigned char *>("Up");
 	const unsigned char* string4 = reinterpret_cast<const unsigned char *>("Down");
-	
-	static unsigned char column_val = 0x01; // sets the pattern displayed on columns
-	static unsigned char column_sel = 0x7F; // grounds column to display pattern
+
+	static unsigned char score = 0;
+	static unsigned char x_pos = 1;
+	static unsigned char y_pos = 1;
 	
 	switch(state) //Transition 
 	{
@@ -224,19 +205,27 @@ void ButtonTick()
 			break;
 	}
 
-	switch(state)//Actions
+	switch(state)
 	{
-		
 		case Init:
+			//LightLED(8,8);
 			x = readadc(4);
 			y = readadc(5);
+			//LightLED(x_pos,y_pos);
+			//LightLED(7,7);
 			if(x > 800) {
 				LCD_DisplayString(1, string1);
-				column_sel = (column_sel << 1) | 0x01;
+				UnlightLED(x_pos, y_pos);
+				x_pos = x_pos + 1;
+				LightLED(x_pos, y_pos);
+				//column_sel = (column_sel >> 1) | 0x80;
 			}
 			else if (x < 200){
 				LCD_DisplayString(1, string2);
-				column_sel = (column_sel >> 1) | 0x80;
+				UnlightLED(x_pos, y_pos);
+				x_pos = x_pos - 1;
+				LightLED(x_pos, y_pos);
+				//column_sel = (column_sel << 1) | 0x01;
 			}
 			else{
 				LCD_ClearScreen();
@@ -244,11 +233,17 @@ void ButtonTick()
 			
 			if(y > 800) {
 				LCD_DisplayString(1, string4);
-				column_val = column_val >> 1;
+				UnlightLED(x_pos, y_pos);
+				y_pos = y_pos - 1;
+				LightLED(x_pos, y_pos);
+				//column_val = column_val << 1;
 			}
 			else if (y < 200){
 				LCD_DisplayString(1, string3);
-				column_val = column_val << 1;
+				UnlightLED(x_pos, y_pos);
+				y_pos = y_pos + 1;
+				LightLED(x_pos, y_pos);
+				//column_val = column_val >> 1;
 			}
 
 			//LCD_Cursor(1);
@@ -260,8 +255,10 @@ void ButtonTick()
 			break;
 	}
 	
-	transmit_data(column_val); // PORTB selects column to display pattern
-	transmit_data_blue(column_sel); // PORTA displays column pattern
+	LightLED(x_pos, y_pos);
+	//LightLED(8, 8);
+	//transmit_data(column_val); // PORTB selects column to display pattern
+	//transmit_data_blue(column_sel); // PORTA displays column pattern
 }
 
 
